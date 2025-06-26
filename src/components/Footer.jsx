a64aa1f25b9661fa8f6b3bcb48ee0492a0887432
liviadfsilva <liviadfsilva@gmail.com>
2025-06-26 16:49:10 -0300

    add Older Reviews button and linked to /book-reviews

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center">
-      <div className="w-full bg-[#e7cbb6] p-4 text-center text-[#54473F]">
+    <footer className="flex flex-col items-center border-t border-[#54473F] border-opacity-10">
+      <div className="w-full p-4 text-center text-[#54473F]">
         © 2025 - Made with ♥️ by Lily
       </div>
     </footer>
--- src/components/Recentposts.jsx
+++ src/components/Recentposts.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Recentposts = () => {
   return (
@@ -20,6 +21,9 @@ const Recentposts = () => {
           <img src="https://starsandvellum.com/wp-content/uploads/2023/08/Haunting-Adeline-768x1024.jpg" alt="-" className="w-full h-full object-cover rounded"></img>
           <h1 className="text-[#AF8260] italic text-3xl font-serif mt-5">Haunting Adeline by H. D. Carlton: Book Review</h1>
         </div>
+      </div>
+      <div className="mt-40 flex">
+        <button className="bg-[#e7cbb6] p-4 py-4 px-8 rounded text-[#54473F] font-semibold text-xl ml-auto mr-32"><Link to="/book-reviews">Older Reviews</Link></button>
       </div>
     </div>
   );
