9a4d497d89cbd7ce3101bf73bebe43794c07f0ff
liviadfsilva <liviadfsilva@gmail.com>
2025-07-01 18:55:32 -0300

    create book review post page

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ import Footer from "./components/Footer"
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Login from "./pages/Login"
+import Login from "./pages/Login";
+import BookReviewPost from "./pages/BookReviewPost";
 
 export default function App() {
   return (
@@ -22,6 +23,7 @@ export default function App() {
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/book-review/post/1" element={<BookReviewPost />} />
           </Routes>
         </main>
         <Footer />
