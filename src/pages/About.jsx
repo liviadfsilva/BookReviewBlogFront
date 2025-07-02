a250bd4800052301304e171e4562bc9e1b64639f
liviadfsilva <liviadfsilva@gmail.com>
2025-07-02 17:13:40 -0300

    add blog owner image and make about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,29 @@ import React from "react";
 
 const About = () => {
     return (
-        <h1 className="text-[#54473F] uppercase italic text-5xl text-center font-serif mt-24 mb-12">The Wraith</h1>
+        <div>
+            <h1 className="text-[#AF8260] text-center animate-bounce mt-14">
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 8 9"
+                className="w-10 h-10 inline-block fill-current text-[#AF8260]"
+            >
+                <path
+                d="M2 0c-.55 0-1.04.23-1.41.59-.36.36-.59.85-.59 1.41 0 .55.23 1.04.59 1.41l3.41 3.41 3.41-3.41c.36-.36.59-.85.59-1.41 0-.55-.23-1.04-.59-1.41-.36-.36-.85-.59-1.41-.59-.55 0-1.04.23-1.41.59-.36.36-.59.85-.59 1.41 0-.55-.23-1.04-.59-1.41-.36-.36-.85-.59-1.41-.59z"
+                transform="translate(0 1)"
+                />
+            </svg>
+            </h1>
+
+            <div className="flex ml-40 mt-6 gap-16">
+                <div className="w-[440px] aspect-[2/3]">
+                    <img src="/images/IMG_7353.jpeg" alt="-" className="w-full h-full object-cover rounded"></img>
+                </div>
+
+                <h2 className="ml-44 text-[#54473F] italic text-5xl text-center font-serif">Hello, World!</h2>
+
+            </div>
+        </div>
     );
 };
 
