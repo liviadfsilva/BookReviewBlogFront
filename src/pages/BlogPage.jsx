2d1c25ec239e758b41be0fab439cac5ef2e7f541
liviadfsilva <liviadfsilva@gmail.com>
2025-07-09 19:00:26 -0300

    add flex-wrap to ReviewsPage.jsx and new reviews

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -17,7 +17,7 @@ const BlogPage = () => {
       </svg>
       </h1>
       <h2 className="text-[#54473F] uppercase italic text-5xl text-center font-serif mt-10 mb-12">LATEST THOUGHTS</h2>
-      <div className="flex justify-center flex-wrap gap-32 mt-8">
+      <div className="flex justify-center flex-wrap gap-x-24 gap-y-32 mt-8">
         <div className="w-[350px] h-[350px]">
           <img src="https://images.unsplash.com/photo-1543722530-d2c3201371e7?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="-" className="w-full h-full object-cover rounded"></img>
           <h1 className="text-[#AF8260] italic text-2xl font-serif mt-4"><Link to="/blog/post/1">Theory of Relativity</Link></h1>
--- src/pages/ReviewsPage.jsx
+++ src/pages/ReviewsPage.jsx
@@ -19,20 +19,35 @@ const ReviewsPage = () => {
 
             <h2 className="text-[#54473F] uppercase italic text-5xl text-center font-serif mt-10 mb-12">All Reviews</h2>
 
-            <div className="flex justify-center gap-24 mt-8">
+            <div className="flex justify-center flex-wrap gap-x-24 gap-y-16 my-8">
                 <div className="w-[340px] aspect-[2/3]">
-                <img src="https://m.media-amazon.com/images/I/71M937NDDtL._UF1000,1000_QL80_.jpg" alt="-" className="w-full h-full object-cover rounded"></img>
-                <h1 className="text-[#AF8260] italic text-2xl font-serif mt-4"><Link to="/book-review/post/1">White horse Black Nights by Evie Marceau: Book Review</Link></h1>
+                    <img src="https://m.media-amazon.com/images/I/71M937NDDtL._UF1000,1000_QL80_.jpg" alt="-" className="w-full h-full object-cover rounded"></img>
+                    <h1 className="text-[#AF8260] italic text-2xl font-serif mt-4"><Link to="/book-review/post/1">White horse Black Nights by Evie Marceau: Book Review</Link></h1>
                 </div>
                 
                 <div className="w-[340px] aspect-[2/3]">
-                <img src="https://m.media-amazon.com/images/I/71f++uAekKL._UF1000,1000_QL80_.jpg" alt="-" className="w-full h-full object-cover rounded"></img>
-                <h1 className="text-[#AF8260] italic text-2xl font-serif mt-4"><Link to="/book-review/post/1">The Fabric of Our Souls by K. M. Moronova: Book Review</Link></h1>
+                    <img src="https://m.media-amazon.com/images/I/71f++uAekKL._UF1000,1000_QL80_.jpg" alt="-" className="w-full h-full object-cover rounded"></img>
+                    <h1 className="text-[#AF8260] italic text-2xl font-serif mt-4"><Link to="/book-review/post/1">The Fabric of Our Souls by K. M. Moronova: Book Review</Link></h1>
                 </div>
 
                 <div className="w-[340px] aspect-[2/3]">
-                <img src="https://m.media-amazon.com/images/I/91zXBCFQSsL.jpg" alt="-" className="w-full h-full object-cover rounded"></img>
-                <h1 className="text-[#AF8260] italic text-2xl font-serif mt-4"><Link to="/book-review/post/1">The Sanatorium by Sarah Pearse: Book Review</Link></h1>
+                    <img src="https://m.media-amazon.com/images/I/91zXBCFQSsL.jpg" alt="-" className="w-full h-full object-cover rounded"></img>
+                    <h1 className="text-[#AF8260] italic text-2xl font-serif mt-4"><Link to="/book-review/post/1">The Sanatorium by Sarah Pearse: Book Review</Link></h1>
+                </div>
+
+                <div className="w-[340px] aspect-[2/3]">
+                    <img src="https://m.media-amazon.com/images/I/81hgMa0Bh1L._SL1500_.jpg" alt="-" className="w-full h-full object-cover rounded"></img>
+                    <h1 className="text-[#AF8260] italic text-2xl font-serif mt-4"><Link to="/book-review/post/1">Lore Olympus Vol. II by Rachel Smythe: Book Review</Link></h1>
+                </div>
+                
+                <div className="w-[340px] aspect-[2/3]">
+                    <img src="https://m.media-amazon.com/images/I/91JxHUo8juL._SL1500_.jpg" alt="-" className="w-full h-full object-cover rounded"></img>
+                    <h1 className="text-[#AF8260] italic text-2xl font-serif mt-4"><Link to="/book-review/post/1">Katie the Catsitter by Colleen AF Venable: Book Review</Link></h1>
+                </div>
+
+                <div className="w-[340px] aspect-[2/3]">
+                    <img src="https://m.media-amazon.com/images/I/91eHSa0sVYL._SL1500_.jpg" alt="-" className="w-full h-full object-cover rounded"></img>
+                    <h1 className="text-[#AF8260] italic text-2xl font-serif mt-4"><Link to="/book-review/post/1">The Never King by Nikki St. Crowe: Book Review</Link></h1>
                 </div>
             </ div>
         </div>
