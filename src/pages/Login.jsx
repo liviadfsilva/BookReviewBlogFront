19fcef2056e92f1a8596b3f23eeee7a70b32cbcc
liviadfsilva <liviadfsilva@gmail.com>
2025-07-10 19:17:21 -0300

    fix Login.jsx form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,43 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+
+    const navigate = useNavigate();
+    
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            
+            navigate("/admin");
+        };
+
     return (
         <div>
             <h1 className="text-[#54473F] uppercase italic text-5xl text-center font-serif mt-24 mb-10 cursor-not-allowed">Admin Only</h1>
 
             <div className="flex justify-center mt-2">
-                <input
-                    className="border border-[#AF8260] rounded text-left py-3 pr-20 pl-2 placeholder-neutral-800 placeholder-opacity-25"
-                    type="email" 
-                    id="email" 
-                    placeholder="admin@only.com">
-                </input>
-            </div>
+                <form onSubmit={handleSubmit} className="flex flex-col gap-y-8">
+                    <input
+                        className="border border-[#AF8260] rounded text-left py-3 pr-20 pl-2 placeholder-neutral-800 placeholder-opacity-25"
+                        type="email" 
+                        id="email" 
+                        placeholder="admin@only.com">
+                    </input>
 
-            <div className="flex justify-center mt-6">
-                <input
-                    className="border border-[#AF8260] rounded text-left py-3 pr-20 pl-2 placeholder-neutral-800 placeholder-opacity-25"
-                    type="password" 
-                    id="password" 
-                    placeholder="What's the hex?">
-                </input>
-            </div>
+                    <input
+                        className="border border-[#AF8260] rounded text-left py-3 pr-20 pl-2 placeholder-neutral-800 placeholder-opacity-25"
+                        type="password" 
+                        id="password" 
+                        placeholder="What's the hex?">
+                    </input>
 
-            <div className="flex justify-center mt-8">
-                <button
-                type="submit"
-                className="bg-[#e7cbb6] p-4 py-3 px-6 rounded text-[#54473F] font-semibold text-base uppercase"><Link to="/admin">Let Me In</Link></button>
+                    <input 
+                    type="submit"
+                    value="Let Me In"
+                    className="bg-[#e7cbb6] p-4 py-3 px-6 rounded text-[#54473F] font-semibold text-base uppercase w-40 mx-auto cursor-pointer">
+                    </input>
+
+                </form>
             </div>
         </div>
     );
