e36f72b9c4e22bf31812cf3e677cab754b28daf5
liviadfsilva <liviadfsilva@gmail.com>
2025-07-14 23:34:38 -0300

    change book quote in QuoteSection.jsx

--- src/components/QuoteSection.jsx
+++ src/components/QuoteSection.jsx
@@ -5,9 +5,9 @@ export default function QuoteSection() {
     <section className="bg-[#f5ebe0] w-full py-12 px-6 my-12">
       <div className="max-w-4xl mx-auto text-center">
         <blockquote className="text-2xl italic text-[#54473F] font-serif">
-          “Books are a uniquely portable magic.”
+          “One of my greatest pleasures is falling into a story someone else has written.”
         </blockquote>
-        <p className="mt-4 text-[#AF8260] font-medium">— Stephen King</p>
+        <p className="mt-4 text-[#AF8260] font-medium">— Nora Roberts</p>
       </div>
     </section>
   );
