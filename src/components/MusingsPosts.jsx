c5d354073fe3879cb50a37d3e8a9ba81b84ac29c
liviadfsilva <liviadfsilva@gmail.com>
2025-08-13 16:26:59 -0300

    feat: fetch data from back-end

--- src/components/MusingsPosts.jsx
+++ src/components/MusingsPosts.jsx
@@ -1,42 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const MusingsPosts = () => {
+  const [posts, setPosts] = useState([]);
+  
+    useEffect(() => {
+      fetch("http://localhost:5001/api/blog-posts/latest")
+      .then((res) => res.json())
+      .then((data) => setPosts(data))
+      .catch((err) => console.error("Error fetching latest review:", err))
+    }, []);
+
   return (
     <div>
       <h1 className="text-[#54473F] uppercase italic text-5xl text-center font-serif mt-10 mb-12">Most Recent Musings</h1>
+
       <div className="flex justify-center gap-16 mt-8">
+        {posts.map((post) => (
+          <div className="w-[350px] h-[350px]">
+            <Link
+            to={`/blog/post/${post.id}`}
+            >
+              <img src={post.post_img} alt="-" className="w-full h-full object-cover rounded"></img>
 
-        <div className="w-[350px] h-[350px]">
-          <Link
-          to="/blog/post/1"
-          >
-            <img src="https://images.unsplash.com/photo-1729660220527-83a3bba2338d?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="-" className="w-full h-full object-cover rounded"></img>
-            <h1 className="text-[#AF8260] italic text-2xl font-serif mt-4">My Favorite Horror Movies</h1>
-            <p className="text-[#AF8260] mt-1">I never get tired of them. Ever!</p>
-          </Link>
+              <h1 className="text-[#AF8260] italic text-2xl font-serif mt-4">{post.title}</h1>
+              <p className="text-[#AF8260] mt-1">{post.subtitle}</p>
+            </Link>
         </div>
-
-        <div className="w-[350px] h-[350px]">
-          <Link
-          to="/blog/post/1"
-          >
-            <img src="https://images.unsplash.com/photo-1729660220527-83a3bba2338d?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="-" className="w-full h-full object-cover rounded"></img>
-            <h1 className="text-[#AF8260] italic text-2xl font-serif mt-4">My Favorite Horror Movies</h1>
-            <p className="text-[#AF8260] mt-1">I never get tired of them. Ever!</p>
-          </Link>
-        </div>
-
-        <div className="w-[350px] h-[350px]">
-          <Link
-          to="/blog/post/1"
-          >
-            <img src="https://images.unsplash.com/photo-1729660220527-83a3bba2338d?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="-" className="w-full h-full object-cover rounded"></img>
-            <h1 className="text-[#AF8260] italic text-2xl font-serif mt-4">My Favorite Horror Movies</h1>
-            <p className="text-[#AF8260] mt-1">I never get tired of them. Ever!</p>
-          </Link>
-        </div>
+        ))}
       </div>
+
       <div className="mt-36 flex justify-center mb-6">
         <button className="bg-[#e7cbb6] p-4 py-3 px-6 rounded text-[#54473F] font-semibold text-base uppercase"><Link to="/blog/all-posts">More Posts ➛</Link></button>
       </div>
--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,55 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Pagination from "../components/Pagination";
 
-const posts = [
-    {
-        id: 1,
-        title: "My Favorite Horror Movies",
-        subtitle: "I never get tired of them. Ever!",
-        image: "https://images.unsplash.com/photo-1729660220527-83a3bba2338d?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-        id: 2,
-        title: "Theory of Relativity",
-        subtitle: "Time is an illusion",
-        image: "https://images.unsplash.com/photo-1543722530-d2c3201371e7?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-        id: 3,
-        title: "The Benefits of Journaling",
-        subtitle: "It can drastically improve your mental health.",
-        image: "https://images.unsplash.com/photo-1513666639414-f795d25747a8?q=80&w=1036&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-        id: 4,
-        title: "Stardew Valley 101",
-        subtitle: "How to master the early game phase.",
-        image: "https://play-lh.googleusercontent.com/He92papZcOmkgTi1sLHXQQb02aoyRtJ8ml96njM_cSAcpHhILvxMjhLix4mYEIKXPq4=w240-h480-rw",
-    },
-    {
-        id: 5,
-        title: "The Lore Behind the Crow",
-        subtitle: "They're called a murder for a reason.",
-        image: "https://images.unsplash.com/photo-1609828435263-e9dc691d630b?q=80&w=2034&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-        id: 6,
-        title: "Inside the Mind of a Cat",
-        subtitle: "What do they think?",
-        image: "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0fGVufDB8fDB8fHww",
-    },
-    {
-        id: 7,
-        title: "The Best Brownie Recipe",
-        subtitle: "It's do die for.",
-        image: "https://images.unsplash.com/photo-1564355808539-22fda35bed7e?q=80&w=2047&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
-    },
-];
-
 const BlogPage = () => {
+  const [posts, setPosts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const postsPerPage = 6;
+
+  console.log("posts state:", posts);
+
+  useEffect(() => {
+      fetch("http://localhost:5001/api/blog-posts/")
+      .then(res => {
+          if (!res.ok) {
+              throw new Error("Failed to fetch posts.");
+          }
+          return res.json();
+      })
+      .then(data => setPosts(data.reverse()))
+      .catch(err => console.error(err));
+  }, []);
 
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
@@ -79,11 +49,11 @@ const BlogPage = () => {
           <div key={post.id} className="w-[350px]">
 
             <Link
-            to="/blog/post/1"
+            to={`/blog/post/${post.id}`}
             >
 
               <div className="h-[350px] overflow-hidden rounded">
-                <img src={post.image} className="w-full h-full object-cover rounded"/>
+                <img src={post.post_img} className="w-full h-full object-cover rounded"/>
             </div>
 
             <h3 className="text-[#AF8260] italic text-2xl font-serif mt-4">
